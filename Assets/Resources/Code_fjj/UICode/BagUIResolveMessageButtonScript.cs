215eb4ec5339132ff400cb302de7a02f608099da
2017ScutFjj <32243303+2017ScutFjj@users.noreply.github.com>
2020-06-28 19:32:38 +0800

    Latest

--- Assets/Resources/Code_fjj/UICode/BagUIResolveMessageButtonScript.cs
+++ Assets/Resources/Code_fjj/UICode/BagUIResolveMessageButtonScript.cs
@@ -17,18 +17,37 @@ public class BagUIResolveMessageButtonScript : MonoBehaviour
             switch (BagUIMessageScript.pastIndex - DataManager.bag.GetItemBag().Count)
             {
                 case 0:
+                    GameScript.EquipmentModel[0].SetActive(false);
+                    GameScript.EquipmentModel[0] = null;
+                    if (GameScript.EquipmentModel[1] != null)
+                    {
+                        GameScript.EquipmentModel[1].SetActive(true);
+                    }
+
                     Buf = DataManager.roleEquipment.GetMainWeapon().GetResolveRareEarth();
                     DataManager.roleEquipment.SetMainWeaponNull();
                     break;
                 case 1:
+                    GameScript.EquipmentModel[1].SetActive(false);
+                    GameScript.EquipmentModel[1] = null;
+                    if (GameScript.EquipmentModel[0] != null)
+                    {
+                        GameScript.EquipmentModel[0].SetActive(true);
+                    }
+
+
                     Buf = DataManager.roleEquipment.GetAlternateWeapon().GetResolveRareEarth();
                     DataManager.roleEquipment.SetAlternateWeaponNull();
                     break;
                 case 2:
+                    GameScript.EquipmentModel[2].SetActive(false);
+                    GameScript.EquipmentModel[2] = null;
                     Buf = DataManager.roleEquipment.GetCuirass().GetResolveRareEarth();
                     DataManager.roleEquipment.SetCuirassNull();
                     break;
                 case 3:
+                    GameScript.EquipmentModel[3].SetActive(false);
+                    GameScript.EquipmentModel[3] = null;
                     Buf = DataManager.roleEquipment.GetHelm().GetResolveRareEarth();
                     DataManager.roleEquipment.SetHelmNull();
                     break;
